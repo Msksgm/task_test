e9248c0f182d48041428c631a43c96f6785e7713
Msksgm <msksgm3124@gmail.com>
2021-09-25 06:55:27 +0900

    add: edit 更新画面

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -83,6 +83,9 @@ class ContactFormController extends Controller
     public function edit($id)
     {
         //
+        $contact = ContactForm::find($id);
+
+        return view('contact.edit', compact('contact'));
     }
 
     /**
